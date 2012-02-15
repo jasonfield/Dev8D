a1570b1d0a2d7ebcf9af2580534fd8988cfbd321
Jason Field <jason@jasonfield.com>
2012-02-15 17:52:44 +0000

    Added sample information for the rows in the CSV file.

--- analyse.py
+++ analyse.py
@@ -3,13 +3,12 @@ import csv
 import pylab
 import numpy as np
 
-
 reader = csv.reader(open("PCA_cooking_oils_CSV.dat", "r"))
 times = np.array(reader.next())
 
 spectra = [np.array(line) for line in reader]
 
-for s in spectra[::5]:
+for s in spectra[::10]:
     pylab.plot(times, s)
 
 pylab.show()
